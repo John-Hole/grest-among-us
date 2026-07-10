fd659c8b31c015146e2ee47e9d5583e5b40db1b7
John Hole <giovannibuco2020@gmail.com>
2026-07-10 15:16:35 +0200

    Add files via upload

--- js/game-logic.js
+++ js/game-logic.js
@@ -9,19 +9,23 @@ export const IMPOSTORS_LIST = ["Ludovica", "Paolo", "Cardoni"];
 export const SCIENTIST_LIST = ["Diego"];
 
 export const TASKS_LIST = [
-    "Canestri",
-    "Trova l'oggetto",
-    "Rebus",
-    "Puzzle",
-    "Pulisci il bagno",
-    "Matematica",
-    "Limbo",
-    "Avanti un altro",
-    "C'entra il bicchiere",
-    "Percorso bendato",
-    "Riempi il bicchiere",
-    "Ricorda la sequenza",
-    "Attacca le orecchie al bianconiglio"
+    "1. Canestri: 3 canestri da tiro libero (canestro oratorio)",
+    "2. Trova l'oggetto: trova 10 oggetti dentro la scatola piena di acqua sporca (panchine rosse)",
+    "3. Rebus: risolvi 2 fogli di rebus (atrio)",
+    "4. Puzzle: componi un puzzle (atrio)",
+    "5. Pulisci il bagno: 1 minuto per pulire tutto il bagno dalla tempera con la spugnetta (bagno donne)",
+    "6. Matematica: risolvi 10 operazioni in 1 minuto e mezzo (ex biblioteca)",
+    "7. Limbo: supera 3 livelli (ex biblioteca)",
+    "8. Avanti un'altro: rispondi a 15 domande (salone)",
+    "9. C'entra il bicchiere: fai c'entro con i pennarelli nel bicchiere in un minuto e mezzo (salone)",
+    "10. Percorso bendato: l'animatore dà le indicazioni al bambino per fare il percorso (strada di fianco oratorio)",
+    "11. Riempi il bicchiere: con un bicchiere da shottino pieno corri per riempire un bicchiere grande, avanti e dietro fino a quando non sarà pieno (strada di fianco oratorio)",
+    "12. Ricorda la sequenza: ripeti la sequenza 1 volta (teatro)",
+    "13. Attacca le orecchie al bianconiglio: 1 minuto per attaccare bendati le orecchie al bianconiglio con 3 tentativi, al terzo possono essere guidati a voce dall'animatore (teatro)",
+    "14. Twister: resisti un minuto e mezzo cambiando posizione (teatro)",
+    "15. Dinosauro: pesca un bigliettino dove c'è scritto il punteggio da raggiungere e poi giocare fino a quando non ci arriva (regia)",
+    "16. Whisper challenge: indovina 5 frasi (corridoio catechismo)",
+    "17. Cruciverba: risolvi un cruciverba (stanza verde catechismo)"
 ];
 
 export const ROUND_TIMES = [
