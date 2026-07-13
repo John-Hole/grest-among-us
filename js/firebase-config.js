dc398b97a417cd768ac2251545c3b8a303a27816
John-Hole <giovannibuco2020@gmail.com>
2026-07-13 14:39:08 +0200

    Fix: Hardcoded Firebase production credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,10 +3,10 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.3/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDP523x9SZZ6MVkvL3tVbuv5SBpbzVsxr4",
+  apiKey: "AIzaSyDP523x9SZZ6MVkvl3tVbuv5S8pbzVsxr4",
   authDomain: "grest-among-us.firebaseapp.com",
-  projectId: "grest-among-us",
   databaseURL: "https://grest-among-us-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "grest-among-us",
   storageBucket: "grest-among-us.firebasestorage.app",
   messagingSenderId: "113254807143",
   appId: "1:113254807143:web:f9491251c65d4d717b46c2"
