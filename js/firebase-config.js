13a8e1f979cb011d8c48db1d9ade4c617815a097
John-Hole <giovannibuco2020@gmail.com>
2026-07-23 16:56:17 +0200

    Directly initialize Firebase config in firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
-import { loadFirebaseConfig } from './env-loader.js';
 
-// Load Firebase config from environment or fallback sources
-const firebaseConfig = await loadFirebaseConfig();
+// Firebase Configuration for realmong-us-g20b
+const firebaseConfig = {
+  apiKey: "AIzaSyBSbg2-cLl3SYG4odF2NYWWDtshoEv6lVo",
+  authDomain: "realmong-us-g20b.firebaseapp.com",
+  databaseURL: "https://realmong-us-g20b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "realmong-us-g20b",
+  storageBucket: "realmong-us-g20b.firebasestorage.app",
+  messagingSenderId: "200595572263",
+  appId: "1:200595572263:web:62f7eeb3cca84df5b7f002",
+  measurementId: "G-EB9910R23E"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
